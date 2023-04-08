dc1ab1fc01ada0314969e0f60a6ec784f41713a6
AMIT-GITHUB2008 <80671746+AMIT-GITHUB2008@users.noreply.github.com>
2023-04-08 22:30:20 +0530

    Update helm.sh

--- helm.sh
+++ helm.sh
@@ -2,123 +2,76 @@
 
 # Add Kubernetes Dashboard repo and install chart
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-helm install my-release kubernetes-dashboard/kubernetes-dashboard
+helm install release-1 kubernetes-dashboard/kubernetes-dashboard
 
 # Add NGINX Ingress Controller repo and install chart
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm install my-release ingress-nginx/ingress-nginx
+helm install release-2 ingress-nginx/ingress-nginx
 
 # Add Prometheus repo and install chart
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm install my-release prometheus-community/prometheus
+helm install release-3 prometheus-community/prometheus
 
 # Add Grafana repo and install chart
 helm repo add grafana https://grafana.github.io/helm-charts
-helm install my-release grafana/grafana
+helm install release-4 grafana/grafana
 
 # Add Elasticsearch repo and install chart
 helm repo add elastic https://helm.elastic.co
-helm install my-release elastic/elasticsearch
+helm install release-5 elastic/elasticsearch
 
 # Add Fluentd repo and install chart
 helm repo add fluent https://fluent.github.io/helm-charts
-helm install my-release fluent/fluentd
+helm install release-6 fluent/fluentd
 
 # Add MySQL repo and install chart
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install my-release bitnami/mysql
+helm install release-7 bitnami/mysql
 
 # Add Redis repo and install chart
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install my-release bitnami/redis
+helm install release-8 bitnami/redis
 
 # Add RabbitMQ repo and install chart
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install my-release bitnami/rabbitmq
+helm install release-9 bitnami/rabbitmq
 
 # Add Jenkins repo and install chart
 helm repo add jenkins https://charts.jenkins.io
-helm install my-release jenkins/jenkins
-#!/bin/bash
+helm install release-10 jenkins/jenkins
 
 # Add PostgreSQL repo and install chart
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install my-release bitnami/postgresql
+helm install release-11 bitnami/postgresql
 
 # Add Minio repo and install chart
 helm repo add minio https://helm.min.io/
-helm install my-release minio/minio
+helm install release-12 minio/minio
 
 # Add Redis Exporter repo and install chart
 helm repo add oliver006 https://oliver006.github.io/helm-charts
-helm install my-release oliver006/redis-exporter
+helm install release-13 oliver006/redis-exporter
 
 # Add Kafka repo and install chart
 helm repo add confluentinc https://confluentinc.github.io/cp-helm-charts/
-helm install my-release confluentinc/cp-kafka
+helm install release-14 confluentinc/cp-kafka
 
 # Add Jenkins X repo and install chart
 helm repo add jenkins-x https://jenkins-x.github.io/jenkins-x-versions/
-helm install my-release jenkins-x/jx
+helm install release-15 jenkins-x/jx
 
 # Add Nexus repo and install chart
 helm repo add sonatype https://sonatype.github.io/helm3-charts/
-helm install my-release sonatype/nexus
+helm install release-16 sonatype/nexus
 
 # Add Elastic APM repo and install chart
 helm repo add elastic https://helm.elastic.co
-helm install my-release elastic/apm-server
+helm install release-17 elastic/apm-server
 
 # Add Argo CD repo and install chart
 helm repo add argo https://argoproj.github.io/argo-helm
-helm install my-release argo/argo-cd
+helm install release-18 argo/argo-cd
 
 # Add Keycloak repo and install chart
 helm repo add codecentric https://codecentric.github.io/helm-charts
-helm install my-release codecentric/keycloak
-
-# Add Graylog repo and install chart
-helm repo add graylog https://helm.graylog.org/
-helm install my-release graylog/graylog
-#!/bin/bash
-
-# Add Kube State Metrics repo and install chart
-helm repo add kube-state-metrics https://prometheus-community.github.io/helm-charts
-helm install my-release kube-state-metrics kube-state-metrics/kube-state-metrics
-
-# Add Vault repo and install chart
-helm repo add hashicorp https://helm.releases.hashicorp.com
-helm install my-release hashicorp/vault
-
-# Add Traefik repo and install chart
-helm repo add traefik https://helm.traefik.io/traefik
-helm install my-release traefik traefik/traefik
-
-# Add MongoDB repo and install chart
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install my-release bitnami/mongodb
-
-# Add Jenkinsfile Runner repo and install chart
-helm repo add jenkins https://charts.jenkins.io
-helm install my-release jenkins/jenkinsfile-runner
-
-# Add Kiali repo and install chart
-helm repo add kiali https://kiali.org/helm-charts
-helm install my-release kiali kiali/kiali
-
-# Add Istio repo and install chart
-helm repo add istio https://istio.io/latest/
-helm install my-release istio-base istio/istio-base
-helm install my-release istiod istio/istiod
-
-# Add Argo Workflows repo and install chart
-helm repo add argo https://argoproj.github.io/argo-helm
-helm install my-release argo/argo-workflows
-
-# Add Cert-Manager repo and install chart
-helm repo add jetstack https://charts.jetstack.io
-helm install my-release jetstack/cert-manager
-
-# Add Nginx Unit repo and install chart
-helm repo add nginx-unit https://nginx.org/packages/helm/chart
-helm install my-release nginx-unit nginx-unit/nginx-unit
+helm install
